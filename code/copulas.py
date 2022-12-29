82ac5adb8b9338acf523d4d111da97da71b46852
MatthewBurke1995 <m.peroburke@gmail.com>
2022-12-29 21:06:06 +0900

    Deployed a3a2fcb with MkDocs version: 1.4.2

--- code/copulas.py
+++ code/copulas.py
@@ -3,34 +3,36 @@ from scipy.stats import norm
 from scipy.stats import kendalltau
 
 
-tickers = yf.Tickers('msft aapl')
+tickers = yf.Tickers("msft aapl")
 df = tickers.history(period="5y")
-aapl_returns = df['Close']['AAPL'].pct_change().dropna()
-msft_returns = df['Close']['MSFT'].pct_change().dropna()
+aapl_returns = df["Close"]["AAPL"].pct_change().dropna()
+msft_returns = df["Close"]["MSFT"].pct_change().dropna()
 
 print("AAPL return distribution")
 print(aapl_returns.describe())
 print("MSFT return distribution")
 print(msft_returns.describe())
 
-def msft_return_probability(returns:float) -> float:
+
+def msft_return_probability(returns: float) -> float:
     """CDF of MSFT returns
     >>> round(msft_return_probability(-0.05),2)
-    0.01
+    0.0
     """
     mean = 0.00115
     std = 0.01947
-    zscore = (returns - mean)/std
+    zscore = (returns - mean) / std
     return norm.cdf(zscore)
 
-def aapl_return_probability(returns:float) -> float:
+
+def aapl_return_probability(returns: float) -> float:
     """CDF of AAPL returns
     >>> round(aapl_return_probability(-0.05),2)
-    0.03
+    0.01
     """
     mean = 0.001252
     std = 0.020985
-    zscore = (returns - mean)/std
+    zscore = (returns - mean) / std
     return norm.cdf(zscore)
 
 
@@ -40,20 +42,20 @@ def independent_copula(u, v):
 
 
 import numpy as np
+
+
 def gumbel_copula(u, v, alpha):
     """Calculate joint probability while accounting for correlation"""
     u = np.asarray(u)
     v = np.asarray(v)
     # compute copula
-    copula = np.exp(-(((-np.log(u))**alpha + (-np.log(v))**alpha))**(1/alpha))
+    copula = np.exp(-((((-np.log(u)) ** alpha + (-np.log(v)) ** alpha)) ** (1 / alpha)))
     return copula
 
 
-
-def calculate_alpha(u,v):
+def calculate_alpha(u, v):
     """Alpha value to be used in gumbel copula"""
-    u=np.array(u)
-    v=np.array(v)
-    tau = kendalltau(u,v).correlation
-    return 1/(1-tau)
-
+    u = np.array(u)
+    v = np.array(v)
+    tau = kendalltau(u, v).correlation
+    return 1 / (1 - tau)
